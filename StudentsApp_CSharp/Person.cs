a2082790e9c724d60b7618a90e43639b662cefa3
Simina Rosioru <simina.rosioru@gmail.com>
2024-03-24 14:41:42 +0200

    created the Person class

---
+++ StudentsApp_CSharp/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsApp_CSharp
+{
+    class Person
+    {
+        //declared variables
+        protected string m_name;
+        protected byte m_age;
+
+        //constructor
+        public Person(string name, byte age)
+        {
+            this.m_name = name;
+            this.m_age = age;
+        }
+    }
+}
