3c95d97c199050bb03a6209e3ae6f3912d1bae7b
Simina Rosioru <simina.rosioru@gmail.com>
2024-03-25 21:04:15 +0200

    created the Student class

---
+++ StudentsApp_CSharp/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsApp_CSharp
+{
+    //the Student class inherits the Person class
+    class Student :Person
+    {
+        private byte year;
+        //variable for the notes vector
+        private byte[] notes = new byte[5];
+
+        //constructor
+        public Student(byte year, byte[] notes, string name, byte age)
+            :base(name, age)
+        {
+            this.year = year;
+            this.notes = notes;
+        }
+
+        
+    }
+}
