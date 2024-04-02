9b4a53951c45d52e4bbcb625eb07504f5a642555
Simina Rosioru <simina.rosioru@gmail.com>
2024-04-02 20:34:59 +0300

    created the event for the Add button
    created the event for the Add button

--- StudentsApp_CSharp/Form1.cs
+++ StudentsApp_CSharp/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +12,45 @@ namespace StudentsApp_CSharp
 {
     public partial class Form1 : Form
     {
+        //created a generic list of objects
+        private List<Student> list_student = new List<Student>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //created the event for the Add button
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            byte[] n_vector = new byte[5];
+
+            try 
+            {
+                n_vector[0] = Convert.ToByte(cmb_Note1.Text);
+                n_vector[1] = Convert.ToByte(cmb_Note2.Text);
+                n_vector[2] = Convert.ToByte(cmb_Note3.Text);
+                n_vector[3] = Convert.ToByte(cmb_Note4.Text);
+                n_vector[4] = Convert.ToByte(cmb_Note5.Text);
+
+                //created the object
+                Student object_s = new Student(Convert.ToByte(cmb_Year.Text), n_vector, 
+                    txt_Name.Text,Convert.ToByte(numericUpDown_Age.Value));
+
+                //added the object_s to the generic list
+                list_student.Add(object_s);
+
+                listDisplay();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listDisplay()
+        {
+            
         }
     }
 }
