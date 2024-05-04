0b3f902f7a9c1db1de1505201add840aa304f50b
Simina Rosioru <simina.rosioru@gmail.com>
2024-05-04 17:54:26 +0300

    Update Person.cs
    added a new variable and changed the constructor of the Person class

--- StudentsApp_CSharp/Person.cs
+++ StudentsApp_CSharp/Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +9,14 @@ namespace StudentsApp_CSharp
     class Person
     {
         //declared variables
+        protected string cnp;
         protected string m_name;
         protected byte m_age;
 
         //constructor
-        public Person(string name, byte age)
+        public Person(string cnp, string name, byte age)
         {
+            this.cnp = cnp;
             this.m_name = name;
             this.m_age = age;
         }
